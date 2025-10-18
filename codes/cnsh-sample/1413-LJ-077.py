2610199187c1e4a905c50d047b8a1d89d6a2087f
생선공장 <gatesplan@gmail.com>
2025-10-19 02:44:11 +0900

    ..

--- codes/cnsh-sample/1413-LJ-077.py
+++ codes/cnsh-sample/1413-LJ-077.py
@@ -12,15 +12,18 @@ def f(x,y,z,d):
             z.pop()
     visited[x] = False
     return
+
 M = [[] for i in range(11)]
-ans = 123456789987654321 
+ans = 123456789987654321
 visited = [0 for i in range(11)]
 ok = False
 V, E = map(int, input().split())
+
 for i in range(E):
     u, v, w = map(int, input().split())
     M[u].append((v,w))
     M[v].append((u,w))
+
 for v in range(0, V+1):
     f(v, 1, [v],0)
 
